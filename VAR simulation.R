639881febd50c8104138930fd362f333d49e20ef
vanfloris <vanfloris@gmail.com>
2022-01-17 14:49:18 +0100

    commit

--- VAR simulation.R
+++ VAR simulation.R
@@ -14,7 +14,7 @@ p <- 2 # Number of lags
 # Generate coefficient matrices
 a <- -0.4
 gamma <- 0.8
-delta <- 0
+delta <- 0.1
 r.delta <- sample(delta, 1) # Create list with random delta's 
 alpha <- t(t(c(a,0,0,0)))
 beta <- t(t(c(1,0,0,0)))
@@ -34,23 +34,37 @@ A <- A.1 + A.2 + B
 # Generate series
 series <- matrix(0, k, t + 2*p) # Raw series with zeros
 for (i in (p + 1):(t + 2*p)){ # Generate series with e ~ N(0,1)
-  series[, i] <- A%*%series[, i-1] + B%*%series[, i-2] + rnorm(k, 0, 1)
+  series[, i] <- A%*%series[, i-1] - B%*%series[, i-2] + rnorm(k, 0, 1)
 }
 
 seriests <- ts(t(series[, -(1:p)])) # Convert to time series format
+ts.plot(seriests)
+transseries <- t(series)
 names <- c("V1", "V2", "V3", "V4") # Rename variables
+colnames(transseries) <- names
 
-## Cointegration test ##
-#logseries <- (series
-ca <- ca.jo(series, type = "trace", K = 2, ecdet = "none")
-summary(ca)
+## Cointegration test using ca.jo##
+ca <- ca.jo(transseries, type = "trace", K = 2, ecdet = "none")
+S = summary(ca)
+str(S)
+teststat <- rev(S@teststat)
+for(i in 1:4){
+  if()
+}
+
+## Cointegration ourselves ##
+tracetest <- function(y,x){-t * sum }
+
+#for(   ){
+#  r = 0 if test < crit value return r = 0
+#  else test r + 1 
+#}
 
 
 # Number of simulations
 nr.sim <- 1000
 # set the true value of the cointegration rank
-mu <- 0
-c <- 0.05
+
 # Initialize a vector of 0s to store rejections
 reject.n <- rep(0, times = nr.sim)
 # Initialize a vector of 0s to store rejections
@@ -63,14 +77,11 @@ for (i in 1:nr.sim){
   # Generate sample from VAR
   seriessim <- matrix(0, k, t + 2*p) # Raw series with zeros
   for (i in (p + 1):(t + 2*p)){ # Generate series with e ~ N(0,1)
-    seriessim[, i] <- A%*%seriessim[, i-1] + B%*%seriessim[, i-2] + rnorm(k, 0, 1)
+    seriessim[, i] <- A%*%seriessim[, i-1] - B%*%seriessim[, i-2] + rnorm(k, 0, 1)
   }
   ##Step 2: Apply Trace test ##
-  X.bar <- mean(seriessim) # Sample Mean
-  St.Dev <- sd(seriessim) # Sample Std. Dev
-  Q.n <- sqrt(n) * X.bar / St.Dev # Test statistic
-  cv.n <- qnorm(1-c) # normal critical value
-  cv.t <- qt(1-c, n-1) # t critical value
+ 
+  
   ## Step 3: Evaluate ##
   # Check if null hypothesis is rejected
   if (Q.n > cv.n) {reject.n[i] <- 1}
@@ -78,12 +89,4 @@ for (i in 1:nr.sim){
 }
 mean(series)
 
-####### Using Ca.Jo ####### 
-dfseries <- data.frame(series)
-ca.jo(cbinddfseries1, type = "trace", K = 2, ecdet = "none")
-dfseries1 <- dfseries[1,]
-dfseries2 <- dfseries[2,]
-dfseries3 <- dfseries[3,]
-dfseries4 <- dfseries[4,]
 
-ca.jo(t(series), type = "trace", K = 4, ecdet = "none")
