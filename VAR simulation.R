dc73fb04188d8be93e14537e1b80450782ff20e7
michiel98fjhsad <94781488+michiel98fjhsad@users.noreply.github.com>
2022-01-17 14:14:27 +0100

    updated the trace test

--- VAR simulation.R
+++ VAR simulation.R
@@ -45,17 +45,22 @@ ts.plot(transseries)
 
 ## Cointegration test ##
 #logseries <- (series
-ca <- ca.jo(transseries, type = "trace", K = 2, ecdet = "none")
-summary(ca)
-
 colnames(transseries) <- names
-colnames(transseries)
+
+ca <- ca.jo(transseries, type = "trace", K = 2, ecdet = "none")
+S <- summary(ca)
+str(S)
+cv <- c(48.28, 31.52, 17.95, 8.18)
+teststats <- rev(S@teststat)
+c.rank <-0
+for(i in 1:4){
+  if(teststats[i]< cv[i]){
+    return(c.rank <- i-1) 
+  }
+}
 
 # Number of simulations
-nr.sim <- 1000
-# set the true value of the cointegration rank
-mu <- 0
-c <- 0.05
+nr.sim <- 10
 # Initialize a vector of 0s to store rejections
 reject.n <- rep(0, times = nr.sim)
 # Initialize a vector of 0s to store rejections
@@ -63,25 +68,36 @@ reject.t <- rep(0, times = nr.sim)
 
 ###### Start the Simulation ######
 
-for (i in 1:nr.sim){
+for (j in 1:nr.sim){
   ## Step 1: Simulate ##
   # Generate sample from VAR
-  seriessim <- matrix(0, k, t + 2*p) # Raw series with zeros
+  series <- matrix(0, k, t + 2*p) # Raw series with zeros
   for (i in (p + 1):(t + 2*p)){ # Generate series with e ~ N(0,1)
-    seriessim[, i] <- A%*%seriessim[, i-1] + B%*%seriessim[, i-2] + rnorm(k, 0, 1)
+    series[, i] <- A%*%series[, i-1] - B%*%series[, i-2] + rnorm(k, 0, 1)
   }
+  
+  seriests <- ts(t(series[, -(1:p)])) # Convert to time series format
+  names <- c("V1", "V2", "V3", "V4") # Rename variables
+  transseries <- t(series)
+  colnames(transseries) <- names
+  
   ##Step 2: Apply Trace test ##
-  X.bar <- mean(seriessim) # Sample Mean
-  St.Dev <- sd(seriessim) # Sample Std. Dev
-  Q.n <- sqrt(n) * X.bar / St.Dev # Test statistic
-  cv.n <- qnorm(1-c) # normal critical value
-  cv.t <- qt(1-c, n-1) # t critical value
+  ca <- ca.jo(transseries, type = "trace", K = 2, ecdet = "none")
+  S <- summary(ca)
+  #str(S)
+  cv <- c(48.28, 31.52, 17.95, 8.18)
+  teststats <- rev(S@teststat)
+  c.rank <-0
+  for(i in 1:4){
+    if(teststats[i]< cv[i]){
+      return(c.rank[j] <- i-1) 
+    }
+  }
   ## Step 3: Evaluate ##
   # Check if null hypothesis is rejected
-  if (Q.n > cv.n) {reject.n[i] <- 1}
-  if (Q.n > cv.t) {reject.t[i] <- 1}
+  #if (Q.n > cv.n) {reject.n[j] <- 1}
+  #if (Q.n > cv.t) {reject.t[j] <- 1}
 }
-mean(series)
 
 
 ########## Trace Test Function ########## 
