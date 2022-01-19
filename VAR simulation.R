52e83b821aec6ad875c5362e2ca4dee7717972b9
Ivan <i.ricardo@student.maastrichtuniversity.nl>
2022-01-19 11:32:38 +0100

    re-centered residuals

--- VAR simulation.R
+++ VAR simulation.R
@@ -32,12 +32,16 @@ names <- c("V1", "V2", "V3", "V4") # Rename variables
 
 ###### Start the Simulation ######
 Q <- matrix(data = NA,nrow= nr.sim, ncol = 4)
+e <- matrix(data = NA, nrow = nr.sim, ncol = 4)
 for (j in 1:nr.sim){
   ## Step 1: Simulate ##
   # Generate sample from VAR
   series <- matrix(0, k, t + 2*p) # Raw series with zeros
+
   for (i in (p + 1):(t + 2*p)){ # Generate series with e ~ N(0,1)
-    series[, i] <- A%*%series[, i-1] - Bmat%*%series[, i-2] + rnorm(k, 0, 1)
+    e_temp <- rnorm(k, 0, 1)
+    e[j,c(1:4)] <- e_temp # residuals are saved into a vector
+    series[, i] <- A%*%series[, i-1] - Bmat%*%series[, i-2] + e_temp # VAR of order 2
   }
   X <- t(series)
   colnames(X) <- names
@@ -61,10 +65,43 @@ ERF.1 <- mean(reject.1)
 # give the output on the screen
   print(paste("Chance to reject 0: ", ERF.0))
   print(paste("Chance to reject 1: ", ERF.1))
+  ts.plot(X)
+
+  
+  
+  
+  
+  ############# OLS function ############# 
+  
+  ols <- function(Y,X){
+    b<- solve(crossprod(X), crossprod(X,Y)) # coefficient estimates
+    y.hat <- X%*%b # fitted values
+    out <- list(coef.estimates = b, fitted.values = y.hat)
+    return(out)
+  }
+  
+  ############# Estimated VAR based on simul  ############# 
+  VARnew <- VAR(X, p = 2, type = "const")
+  res.VARnew <- residuals(VARnew)
+
+  mean.res.VARnew <- sumq/t
+  
+  
+  # mean matrix of residuals
+  mean.res <- cbind(mean(res.VARnew[,1]), mean(res.VARnew[,2]), mean(res.VARnew[,3]), mean(res.VARnew[,4])) 
+  mean.res1 <- mean(res.VARnew[,1]); mean.res2 <- mean(res.VARnew[,2]); mean.res3 <- mean(res.VARnew[,3]); mean.res4 <- mean(res.VARnew[,4])
+  
+  # re-centered residuals
+  res.cen <- cbind(res.VARnew[,1] - mean.res1, res.VARnew[,2] - mean.res2, res.VARnew[,3] - mean.res3, res.VARnew[,4] - mean.res4) 
 
   
   
   ##################### THE BOOTSTRAP IN R #####################
+  
+  
+  
+  
+  
   # First draw indices of the bootstrap sample: draw n times with replacement
   n = 54
   J <- ceiling(runif(n, min = 0, max = n))
@@ -87,7 +124,7 @@ ERF.1 <- mean(reject.1)
     cv.star2 <- quantile(Q.star1[,2], probs=0.95)
     #if (teststats.star[1] > 48.28) {reject.star.0[b] <- 1}
     #if (teststats.star[2] > 31.52) {reject.star.1[b] <- 1}
-  
+ts.plot(Q.star1)  
   
 
  ######### Testing for the Trace test #########
